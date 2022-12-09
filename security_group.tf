bf6613c046592297ee88d0695d23724f846d873c
Ngô Văn Toàn <toango123zx@gmail.com>
2022-12-09 14:02:44 +0700

    tao security group EC2SG, dbsg

---
+++ security_group.tf
@@ -0,0 +1,80 @@
+resource "aws_security_group" "EC2SG" {
+  name = "EC2SG"
+  tags = {
+        Name = "public"
+  }
+  description = "ssh"
+  vpc_id = "${aws_vpc.MyVpc.id}"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "ICMP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "dbsg" {
+  name = "dbsg"
+  tags = {
+        Name = "private"
+  }
+  description = "MYSQL Traffic"
+  vpc_id = "${aws_vpc.MyVpc.id}"
+  ingress {
+      from_port = 3306
+      to_port = 3306
+      protocol = "TCP"
+      security_groups = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
