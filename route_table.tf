d838cef15cfbb30cd0caf1cf35c5523c3b016a50
Ngô Văn Toàn <toango123zx@gmail.com>
2022-12-09 14:04:52 +0700

    tao route table public, private

---
+++ route_table.tf
@@ -0,0 +1,32 @@
+resource "aws_route_table" "MyVpc_route_table_public" {
+  vpc_id = "${aws_vpc.MyVpc.id}"
+  tags = {
+      Name = "Public route Table"
+  }
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.MyVPC_internet_gateway.id}"
+    }
+}
+
+resource "aws_route_table_association" "Public" {
+    subnet_id_1 = "${aws_subnet.MyVPC_subnet_public_1.id}"
+    subnet_id_2 = "${aws_subnet.MyVPC_subnet_public_2.id}"
+    route_table_id = "${aws_route_table.MyVpc_route_table_public.id}"
+}
+
+resource "aws_route_table" "MyVpc_route_table_private" {
+  vpc_id = "${aws_vpc.MyVpc.id}"
+  tags = {
+      Name = "Private route table"
+  }
+#   route {
+#         cidr_block = "0.0.0.0/0"
+#         nat_gateway_id = "${aws_nat_gateway.NatGw.id}"
+#   }
+}   
+
+resource "aws_route_table_association" "Private" {
+    subnet_id = "${aws_subnet.MyVPC_subnet_private.id}"
+    route_table_id = "${aws_route_table.MyVpc_route_table_private.id}"
+}
