fe005aa3069ce6e05efd57bc87b741cedabc862f
toango123zx <toango123zx@gmail.com>
2022-12-09 16:17:13 +0700

    done security group

--- security_group.tf
+++ security_group.tf
@@ -49,7 +49,6 @@ resource "aws_security_group" "EC2SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   egress {
     from_port = 0
     to_port = 0
@@ -66,10 +65,10 @@ resource "aws_security_group" "dbsg" {
   description = "MYSQL Traffic"
   vpc_id = "${aws_vpc.MyVpc.id}"
   ingress {
-      from_port = 3306
-      to_port = 3306
-      protocol = "TCP"
-      security_groups = ["0.0.0.0/0"]
+      from_port   = 80
+      to_port     = 80
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port = 0
