acb1e4174d1190c421e230150d1caf9cdc5a7c82
toango123zx <toango123zx@gmail.com>
2022-12-09 17:47:54 +0700

    done db subnet group

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,10 +1,10 @@
 resource "aws_db_subnet_group" "dbsubnetgroup" {
   name       = "dbsubnetgroup"
-  availability_zone = ["${var.Availability_zone_1}", "${var.Availability_zone_2}"]
-  subnet_ids = [aws_subnet.MyVPC_subnet_private_1.id, aws_subnet.MyVPC_subnet_private_1.id]
-  vpc_id = "${aws_vpc.MyVpc.id}"
+#   availability_zone = ["${var.Availability_zone_1}", "${var.Availability_zone_2}"]
+  subnet_ids = ["${aws_subnet.MyVPC_subnet_private_1.id}", "${aws_subnet.MyVPC_subnet_private_2.id}"]
   tags = {
     Name = "dbsubnetgroup"
+    vpc_id = "${aws_vpc.MyVpc.id}"
   }
   description = "dbsubnetgroup"
 }
