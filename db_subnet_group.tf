738a0c6cad848936a16a3ef4a463bd5bb252cd9c
Ngô Văn Toàn <toango123zx@gmail.com>
2022-12-09 14:17:57 +0700

    sua chinh ta

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,4 +1,4 @@
-esource "aws_db_subnet_group" "dbsubnetgroup" {
+resource "aws_db_subnet_group" "dbsubnetgroup" {
   name       = "dbsubnetgroup"
   availability_zone = ["${var.Availability_zone_1}", "${var.Availability_zone_2}"]
   subnet_ids = [aws_subnet.MyVPC_subnet_private_1.id, aws_subnet.MyVPC_subnet_private_1.id]
