d46e9b2ec57ea3689673b2d727c671826646790a
toango123zx <toango123zx@gmail.com>
2022-12-09 17:48:34 +0700

    done RDS

--- database.tf
+++ database.tf
@@ -1,17 +1,16 @@
 resource "aws_db_instance" "mysqltest" {
-    vpc_security_group_ids = "${aws_vpc.MyVpc.id}"
     identifier = "mysqltest"
     instance_class       = "db.t2.micro"
     allocated_storage    = 20
     max_allocated_storage = 0   
-    auto_minor_version_upgrade = false7
+    auto_minor_version_upgrade = false
     engine               = "MySQL"
-    engine_version       = "MySQL 8.0.28"
+    engine_version       = "8.0.28"
     username             = "mysqltest" 
     password             = "mysqltest"
+    vpc_security_group_ids = ["${aws_security_group.dbsg.id}"]
     db_subnet_group_name = "${aws_db_subnet_group.dbsubnetgroup.id}"
-    security_group_names = "${aws_security_group.dbsg.id}"
-    snapshot_identifier = false
+    snapshot_identifier = null
     db_name = "mydb"
-    skip_final_snapshot  = false
+    skip_final_snapshot  = true
 }   
