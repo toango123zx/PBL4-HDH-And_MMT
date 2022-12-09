d0e1d8b3e4b34fca1bb5fc02ea12337bb4a35787
toango123zx <toango123zx@gmail.com>
2022-12-09 18:09:57 +0700

    them 2 subnet private vao route table private

--- route_table.tf
+++ route_table.tf
@@ -34,7 +34,16 @@ resource "aws_route_table" "MyVpc_route_table_private" {
 #   }
 }   
 
-# resource "aws_route_table_association" "Private" {
-#     subnet_id = "${aws_subnet.MyVPC_subnet_private_1.id}"
-#     route_table_id = "${aws_route_table.MyVpc_route_table_private.id}"
-# }
+resource "aws_route_table_association" "Public1" {
+    # subnet_id_1 = "${aws_subnet.MyVPC_subnet_public_1.id}"
+    # subnet_id_2 = "${aws_subnet.MyVPC_subnet_public_2.id}"
+    subnet_id = "${aws_subnet.MyVPC_subnet_private_1.id}"
+    route_table_id = "${aws_route_table.MyVpc_route_table_public.id}"
+}
+
+resource "aws_route_table_association" "Public2" {
+    # subnet_id_1 = "${aws_subnet.MyVPC_subnet_public_1.id}"
+    # subnet_id_2 = "${aws_subnet.MyVPC_subnet_public_2.id}"
+    subnet_id = "${aws_subnet.MyVPC_subnet_private_2.id}"
+    route_table_id = "${aws_route_table.MyVpc_route_table_public.id}"
+}
