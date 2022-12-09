5be6082ec077f02b645aca9eadd519331691bb65
Ngô Văn Toàn <toango123zx@gmail.com>
2022-12-09 13:54:44 +0700

    tao security group EC2SG, dbsg

---
+++ db_subnet_group.tf
@@ -0,0 +1,10 @@
+esource "aws_db_subnet_group" "dbsubnetgroup" {
+  name       = "dbsubnetgroup"
+  availability_zone = ["${var.Availability_zone_1}", "${var.Availability_zone_2}"]
+  subnet_ids = [aws_subnet.MyVPC_subnet_private_1.id, aws_subnet.MyVPC_subnet_private_1.id]
+  vpc_id = "${aws_vpc.MyVpc.id}"
+  tags = {
+    Name = "dbsubnetgroup"
+  }
+  description = "dbsubnetgroup"
+}
